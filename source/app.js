96759cf92af874b5268d822615dd23769a4a8384
Romain Picard <romain.picard@softathome.com>
2016-03-10 18:40:18 +0100

    Added support for the load url parameter to load a file on startup

--- source/app.js
+++ source/app.js
@@ -4,12 +4,41 @@
 	it in a call to enyo.ready().
 */
 
+function parse_args(val) {
+    var result = null,
+        tmp = [];
+    location.search
+    //.replace ( "?", "" )
+    // this is better, there might be a question mark inside
+    .substr(1)
+        .split("&")
+        .forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+}
+
 enyo.kind({
 	name: "myapp.Application",
 	kind: "enyo.Application",
-	view: "myapp.MainView"
+	view: "myapp.MainView",
+
+	loadUrl: function(url) {
+		this.view.loadUrl(url);
+	},
+
+	/*
+	viewReadyChanged: function() {
+		console.log("Changed");
+	}
+	*/
 });
 
 enyo.ready(function () {
-	new myapp.Application({name: "app"});
+	var app = new myapp.Application({name: "app"});
+	loadUrl = parse_args("load");
+	if(loadUrl != null) {
+		app.loadUrl(loadUrl);
+	}
 });
--- source/views/views.js
+++ source/views/views.js
@@ -26,13 +26,21 @@ enyo.kind({
 	create: function() {
         this.inherited(arguments);
     },
+
+	loadUrl: function(url) {
+		console.log(loadUrl);
+		d3.json(url, enyo.bind(this, function(error, json) {
+		  if (error) return this.dataError(error);;
+		  this.dataLoaded(json);
+	  }));
+	},
+
     customSelected: function(inSender, inEvent) {
         var file = inEvent.files[0];
         var reader = new FileReader();
 
         reader.onload = enyo.bind(this, "fileLoaded");
         reader.readAsText(file);
-
 	},
 
     fileLoaded: function(e) {
@@ -41,15 +49,24 @@ enyo.kind({
             data = JSON.parse(e.target.result);
         }
         catch(e) {
-            console.log(e);
-            this.$.errorPopup.setContent(e);
-            this.$.errorPopup.show();
+			this.dataError(e);
             return;
         }
 
+		this.dataLoaded(data);
+    },
+
+
+	dataError: function(e) {
+		this.$.errorPopup.setContent(e);
+		this.$.errorPopup.show();
+	},
+
+	dataLoaded: function(data) {
         this.$.plot.setData(data);
         this.$.plot.plot();
     },
+
 
 	clearFilterTapped: function(inSender, inEvent) {
 		this.$.plot.clearFilter();
